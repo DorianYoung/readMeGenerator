d2fcc679d31a47d6cb5e72fe18afcf298b903a5e
Dorian Young <DorianYoung@Ymail.com>
2020-03-02 18:48:36 -0800

    functional. need to clean up code

--- index.js
+++ index.js
@@ -3,20 +3,128 @@ const axios = require("axios");
 const inquirer = require("inquirer");
 
 
-inquirer
-    .prompt({
-        message: "Enter your GitHub username",
-        name: "username"
-    })
 
-    .then(function({ username }) {
-        const queryUrl = `https://api.github.com/users/${username}`;
+ 
+
+
+inquirer
+    .prompt([{
+        message: "Please enter your Github username",
+        name: "username"
+    },
+    {
+        message: "Please enter what you would like inside your projects badge",
+        name: "badge"
+    },
+    {
+        message: "What is your projects title?",
+        name: "projectTitle"
+    },
+    {
+        message: "Write a description for your project",
+        name: "projectDescription"
+    },
+    {
+        message: "Include instructions for project installation",
+        name: "projectInstalation"
+    },
+    {
+        message: "Write a description of your projects usage",
+        name: "projectUsage"
+    },
+    {
+        message: "Include any licenses for your project",
+        name: "projectLicense"
+    },
+    {
+        message: "Include any contributions for your project",
+        name: "projectContribution"
+    }])
+
+    .then(function(result) {
+        const queryUrl = `https://api.github.com/users/${result.username}`;
+        const html = 
+`# ${result.projectTitle}
+
+
+
+
+
+![${result.username}](PUT URL FOR IMAGE HERE)
+https://github.com/${result.username}
+    
+<h2>${result.projectTitle}</h2>
+    
+# ${result.projectTitle}
+${result.projectDescription}
+    
+    
+>  ## **Table of Contents** :notebook:
+    
+    
+* [Installation](#Installation):wrench:
+* [Usage](#Usage):bulb:
+* [Contributions](#Contributions):point_up:
+* [License](#License):lock_with_ink_pen:
+* [Badges](#Badges):warning:
+    
+    
+    
+> ## Installation
+    
+${result.projectInstalation}
+    
+    
+> ## Usage
+    
+${result.projectUsage}
+    
+    
+> ## Contributions
+    
+- OpenWeather API
+    
+${result.projectContribution}
+    
+    
+
+> ## License
+    
+${result.projectLicense}
+    
+    
+    
+> ## Badges
+    
+![${result.badge}](https://img.shields.io/badge/Dorian-100%25-green)
+    
+`
+        
         axios
             .get(queryUrl)
             .then(function(res){
-                console.log(res.data.avatar_url);
-                console.log(res.data.email);
+                var bioPic = res.data.avatar_url;
+                var eMail = res.data.email;
+                
+                console.log(bioPic);
+                console.log(eMail);
+                
+                fs.writeFile('README.md', html , function (err) {
+                    if (err) throw err;
+                    console.log('Your README has been generated!');
+                  });
+                
+                
+                
+                
+                
             });
+
         });
 
+       
+
  
+
+
+        
